515d3b792e73de99a85bda0b7a326e0215820e32
ziadezz <ze01091510222@gmail.com>
2020-08-31 02:34:50 +0200

    index

---
+++ index.h
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title> This is my first website</title>
+<meta name="author" content"zezo"/>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="style.css/>
+<script>
+alert("sss");
+</script>
+
+<style>
+body {background: #080}
+
+</style>
+
+</head>
+<body>
+<!-- comment -->
+<h3> Who is Al_Sisi</h3>
+<hr />
+<p> He is a murderer </p>
+
+</body>
+</html>
